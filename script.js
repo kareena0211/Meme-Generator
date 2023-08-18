5d803c8924671d95d030c34523faa8c1bad58673
kareena22 <kareena22@navgurukul.org>
2023-08-18 10:33:07 +0530

    +1

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
-const generateMemeBtn = document.querySelector(
-    ".meme-generator .generate-meme-btn"
-);
+const generateMemeBtn = document.querySelector(".meme-generator .generate-meme-btn");
 
 const memeImage = document.querySelector(".meme-generator img");
 const memeTitle = document.querySelector(".meme-generator .meme-title");
-const memeAuthor = document.querySelector(".meme-generator .meme-author");
-
+const memeAuthor = document.querySelector("meme-generator .meme-author");
 
 const updateDetails = (url, title, author) => {
     memeImage.setAttribute("src", url);
@@ -13,13 +10,13 @@ const updateDetails = (url, title, author) => {
     memeAuthor.innerHTML = author;
 }
 
-
-const generateMeme = () => {
+const generateMeme=()=>{
     fetch("https://meme-api.com/gimme/wholesomememes")
-        .then((response) => response.json())
-        .then((data) => {
-            updateDetails(data.url, data.title, data.author);
-        });
+    .then((response)=>response.json())
+    .then((data)=>{
+        updateDetails(data.url,data.title,data.author);
+    });
 };
 
-generateMemeBtn.addEventListener('click', generateMeme);
+generateMemeBtn.addEventListener("click",generateMeme);
+
